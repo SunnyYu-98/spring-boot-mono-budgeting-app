cf8ce1fdb465617e023079cb923d1b8ae6297ff3
Sunny Yu <syu1682@outlook.com>
2019-10-30 23:24:15 -0500

    - Initial project setup
    Signed-off-by: Sunny Yu <syu1682@outlook.com>

---
+++ src/main/java/com/cashu/budgetapp/BudgetApplication.java
@@ -0,0 +1,13 @@
+package com.cashu.budgetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/cashu/budgetapp/Controller.java
@@ -0,0 +1,17 @@
+package com.cashu.budgetapp;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @GetMapping("/hello")
+    public String test(HttpServletRequest request, Model model){
+        return "hello";
+    }
+}
