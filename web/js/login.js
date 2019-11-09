bd6ee3ec201b3aa46a4bb3d3ba6e0e2fbd5ffc76
ayrlin.renata <ayrlin.renata@gmail.com>
2019-11-09 21:33:12 +0000

    Added basic site flowthrough to login for dev
    Made it possible to bypass the login page for development purposes.
    Added landing page WIP

---
+++ web/js/login.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+	
+});
+
+/**
+* Creates a login attempt
+*/
+function loginSubmit() {
+	
+	//login success, navigate to landing document.
+	window.location.href = "landing.html";
+}
