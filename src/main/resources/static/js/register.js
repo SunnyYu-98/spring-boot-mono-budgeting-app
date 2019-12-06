6cacf11e97aa9b5076b2e5aeec0370a4f94bb29b
Sunny Yu <syu1682@outlook.com>
2019-12-05 20:32:17 -0600

    - Input validation for first name, last name, and email for registration form

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -12,38 +12,59 @@ $(document).on('change', '.js-birth-date', function () {
     }
 });
 
-$(document).on('change', '.js-email input', function() {
-    $.ajax({
-        url: "/verify-email?email=" + $('.js-email input').val()
-    }).done(function (data){
-        if(data.emailAlreadyTaken){
-            $('.js-email .error').show();
-        }
-        else{
-            $('.js-email .error').hide();
-        }
-    });
-});
-
 $(document).on('change', 'input', function(event) {
 
     const eventClassList = event.target.parentNode.classList;
+    const lettersAndSpaceRegex = /^[A-Za-z ]+$/;
 
-    if(eventClassList.contains("js-firstName")){
-        //$('.js-firstName input').val().match("[a-zA-Z]*")[0] === ""
-        //wont work for first names with multiple names
-        if($('.js-firstName input').val()){
+    if (eventClassList.contains("js-firstName")) {
+        let $input = $('.js-firstName input');
+
+        //Replace extra spaces with one space, and trim trailing spaces
+        $input.val($input.val().replace(/\s+/g, ' ').trim());
+        if (!$input.val().match(lettersAndSpaceRegex)) {
             $('.js-firstName .error').show();
+        } else {
+            $('.js-firstName .error').hide();
         }
     }
 
-    if(eventClassList.contains("js-lastName")){
-        $('.js-lastName .error').show();
+    if (eventClassList.contains("js-lastName")) {
+        let $input = $('.js-lastName input');
+
+        //Replace extra spaces with one space, and trim trailing spaces
+        $input.val($input.val().replace(/\s+/g, ' ').trim());
+        if (!$input.val().match(lettersAndSpaceRegex)) {
+            $('.js-lastName .error').show();
+        } else {
+            $('.js-lastName .error').hide();
+        }
     }
 
-    if(eventClassList.contains("js-email")){
-        $('.js-email .error').show();
+    if (eventClassList.contains("js-email")) {
+        let $input = $('.js-email input');
+
+        //Replace extra spaces with one space, and trim trailing spaces
+        $input.val($input.val().replace(/\s+/g, '').trim());
+
+        if (!$input.val().match(/^[A-Za-z0-9._@]+$/)) {
+            $('.js-email .inputError').show();
+        } else {
+            $('.js-email .inputError').hide();
+        }
+
+        $.ajax({
+            url: "/verify-email?email=" + $input.val()
+        }).done(function (data){
+            if(data.emailAlreadyTaken){
+                $('.js-email .emailTakenError').show();
+            }
+            else{
+                $('.js-email .emailTakenError').hide();
+            }
+        });
     }
+
 
     /*
     switch (eventClassList) {
