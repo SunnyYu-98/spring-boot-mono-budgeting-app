1e051b59e7f35db8e5a90332ec435199cb908237
AustinSoles21 <austincsoles21@gmail.com>
2019-12-08 20:03:38 -0600

    configs

--- src/main/java/com/cashu/budgetapp/service/EmailService.java
+++ src/main/java/com/cashu/budgetapp/service/EmailService.java
@@ -1,28 +1,18 @@
 package com.cashu.budgetapp.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-import org.springframework.mail.javamail.MimeMessageHelper;
-//import sun.nio.ch.DatagramChannelImpl;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-import java.io.IOException;
 
 public class EmailService {
     void sendEmail() {
 
         SimpleMailMessage msg = new SimpleMailMessage();
-        msg.setTo("1@gmail.com", "2@yahoo.com");
+        msg.setTo("austincsoles21@gmail.com");
 
         msg.setSubject("Testing from Spring Boot");
-        msg.setText("Hello World \n Spring Boot Email");
+        msg.setText("Hello World \n This is a test");
 
 
         JavaMailSender javaMailSender = new JavaMailSenderImpl();
