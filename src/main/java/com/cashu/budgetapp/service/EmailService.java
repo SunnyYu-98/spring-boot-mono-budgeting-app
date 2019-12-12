9d2dd70ee71ba8d7d658ca993571fa95aeebcd31
AustinSoles21 <austincsoles21@gmail.com>
2019-12-12 00:12:01 -0600

    - Add EmailService to be able to send email

--- src/main/java/com/cashu/budgetapp/service/EmailService.java
+++ src/main/java/com/cashu/budgetapp/service/EmailService.java
@@ -1,23 +1,58 @@
 package com.cashu.budgetapp.service;
 
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.stereotype.Service;
 
 
+@Service("emailService")
 public class EmailService {
-    void sendEmail() {
+    @Autowired
+    private JavaMailSender javaMailSender;
 
-        SimpleMailMessage msg = new SimpleMailMessage();
-        msg.setTo("austincsoles21@gmail.com");
+    public void sendEmail() {
 
-        msg.setSubject("Testing from Spring Boot");
-        msg.setText("Hello World \n This is a test");
+        final String username = "cashubudgeting@gmail.com";
+        final String password = "cashuDBU2019";
 
+        Properties prop = new Properties();
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "587");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.starttls.enable", "true"); //TLS
 
-        JavaMailSender javaMailSender = new JavaMailSenderImpl();
-        javaMailSender.send(msg);
+        Session session = Session.getInstance(prop,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
 
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("cashubudgeting@gmail.com"));
+            message.setRecipients(
+                    Message.RecipientType.TO,
+                    InternetAddress.parse("sunny1998830@hotmail.com, austincsoles21@gmail.com")
+            );
+            message.setSubject("Testing Gmail TLS");
+            message.setText("Hello appreciated Customer,"
+                    + "\n\n Click this link to change your password.");
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
     }
 
 }
